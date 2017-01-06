760f40b6818a1f179c6681bec2833a36fb4fea3d
Shigeo Kikuchi <kikuchi@shouyabusachi-no-MacBook-Pro.local>
2017-01-06 20:12:08 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Shigeo Kikuchi on 2017/01/06.
+//  Copyright © 2017年 shigeo.kikuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
